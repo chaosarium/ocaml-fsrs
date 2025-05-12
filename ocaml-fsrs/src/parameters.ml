9db4519eda37b6ee4e0af01883fe60b1c28e08a5
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-05-11 23:43:20 -0700

    Add comment explaining initial stability values in weights

--- ocaml-fsrs/src/parameters.ml
+++ ocaml-fsrs/src/parameters.ml
@@ -1,7 +1,8 @@
 type weights = float array
 
 let _DEFAULT_WEIGHTS : weights = [| 
-  0.4072; 1.1829; 3.1262; 15.4722; 7.2102; 
+  0.4072; 1.1829; 3.1262; 15.4722; (* initial stability values for the 4 possible ratings *)
+  7.2102; 
   0.5316; 1.0651; 0.0234; 1.6160; 0.1544; 
   1.0824; 1.9813; 0.0953; 0.2975; 2.2042; 
   0.2407; 2.9466; 0.5034; 0.6567; 
