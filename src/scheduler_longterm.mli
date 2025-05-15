bc5c5b6a24b90fff0d3a0e07fee4553b5f125c3e
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2025-05-14 23:02:38 -0600

    rid of mli files

--- src/scheduler_longterm.mli
+++
@@ -1,7 +0,0 @@
-open Models
-
-(* return a map from possible ratings to the new schedule the schedule will result in *)
-val preview : Scheduler.t -> record_log
-
-(* take a rating and update the card and the new scheduling state *)
-val review : Scheduler.t -> rating -> (Scheduler.t * scheduling_info)
